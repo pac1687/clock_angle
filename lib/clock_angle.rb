5812c1da246abb166e08760d3c6a4f6134a8cd9c
Jeremy Whitaker & Justin Pacubas <student@epicodus.com>
2014-07-23 15:59:59 -0700

    Provides correct distance value for 12th hour

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -2,6 +2,10 @@ def clock_angle(hourHand, minuteHand)
   hourHand*=30
   minuteHand*=6
 
+  if hourHand == 360
+     hourHand*=0
+  end
+
   result=(hourHand-minuteHand).abs
 
   if result > 180
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -4,5 +4,9 @@ require("clock_angle")
 describe("clock_angle") do
   it("whenever the hands are at the same position, it returns a distance of 0") do
       clock_angle(6, 30).should(eq(0))
+  it("returns the shortest distance angle to 0") do
+      clock_angle(9, 00).should(eq(90))
+  it("returns accurate distance value of 0 for hour 12 ") do
+      clock_angle(12, 00).should(eq(0))
   end
 end
