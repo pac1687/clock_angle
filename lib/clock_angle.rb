14507c7fc471226b077795940a466360f27890c7
Jeremy Whitaker & Justin Pacubas <student@epicodus.com>
2014-07-23 15:58:28 -0700

    Provides logic for shortest distance to 0

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,3 +1,13 @@
 def clock_angle(hourHand, minuteHand)
+  hourHand*=30
+  minuteHand*=6
 
+  result=(hourHand-minuteHand).abs
+
+  if result > 180
+    result-=180
+  end
+  result
 end
+
+# clock_angle(3,23)
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -2,7 +2,7 @@ require("rspec")
 require("clock_angle")
 
 describe("clock_angle") do
-  it("") do
-
+  it("whenever the hands are at the same position, it returns a distance of 0") do
+      clock_angle(6, 30).should(eq(0))
   end
 end
